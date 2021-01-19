f6b1ea522d37eb3e8638ecf8616462715c4fc347
algakbk <32064765+algakbk@users.noreply.github.com>
2021-01-19 22:51:41 +0600

    Book model and html page logic added

--- main/models.py
+++ main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    subtitle = models.CharField(max_length=100)
+    description = models.CharField(max_length=1000)
+    price = models.IntegerField()
+    genre = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    year = models.IntegerField()
+    created_at = models.DateField(auto_now_add=True)
--- main/views.py
+++ main/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect
+from .models import Book
 
 # Create your views here.
+
+def homepage(request):
+    book_list = Book.objects.all()
+    return render(request, "index.html", {"book_list": book_list})
